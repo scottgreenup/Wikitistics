3eb57bbbbe5c9ded9847e72709c0f208cc96261c
scottgreenup <scott.j.greenup@gmail.com>
2017-05-27 16:56:26 +1000

    Winston and importing historical data.

--- main.js
+++ main.js
@@ -15,6 +15,7 @@
 //
 //==========================================================================
 
+const async = require('async')
 const fs = require('fs')
 const ejs = require('ejs')
 const express = require('express')
@@ -23,6 +24,9 @@ const mongoose = require('mongoose')
 const path = require('path')
 const request = require('request')
 const urlencode = require('urlencode')
+const winston = require('winston')
+
+mongoose.Promise = global.Promise;
 
 var app = express()
 
@@ -111,62 +115,102 @@ class Article {
     }
 }
 
-var articleMap = new Map();
+function latestTimestamp(revisions) {
 
-function processArticleFile(filename, RevisionCls) {
+    var latest = 0;
+    var revid = null;
 
-    // TODO read in all data asynchronously and then process
+    revisions.forEach(function(r) {
+        var unixTime = moment(r.timestamp).unix();
+        if (unixTime > latest) {
+            latest = unixTime;
+            revid = r.revid;
+        }
+    });
+
+    return {latest: latest, revid: revid}
+}
+
+function processArticleFile(filename, RevisionCls, callback) {
+
+    // Load and sort the historical data by revid
     var data = JSON.parse(fs.readFileSync(dataDir + '/' + filename, 'utf8'));
-    var article = new Article(filename, data)
+    data.sort(function(a, b) {
+        return a.revid - b.revid;
+    });
 
-    //articleMap.set(article.name, article);
+    var article = new Article(filename, data)
+    var tasks = [];
+
+    // Find all revisions in the database for this article, then see if our
+    // historical data has any elements missing from the database. If it does,
+    // add it.
+    RevisionCls
+        .find({title: article.title})
+        .sort({"revid": 1})
+        .exec(function(err, revisions) {
+            var i = 0;
+
+            data.forEach(function(r) {
+                while (i < revisions.length && r.revid > revisions[i].revid) {
+                    i++;
+                }
+
+                if (i < revisions.length) {
+                    if (r.revid == revisions[i].revid) {
+                        return;
+                    }
+                }
+
+                // add r to the database
+                tasks.push(function(callback) {
+                    revision = new RevisionCls(r);
+                    revision.save(function(error, revision) {
+                        if (error) {
+                            console.error(error);
+                        }
+
+                        callback(null, revision);
+                    });
+                });
+            });
+
+            // Run every task in parallel, when they are all finished, execute
+            // the callback for processArticleFile
+            async.parallel(tasks, function(error, results) {
+                winston.info(
+                    "Processed '%s': inserted %d; found %d existing.",
+                    article.title, results.length, revisions.length);
+                callback(error, results.length);
+            });
+        });
+
+    /*
+    var count = 1;
+    var tasks = [];
 
     data.forEach(function(rev) {
+        if ('suppressed' in rev) {
+            return;
+        }
+
         revision = new RevisionCls(rev);
+        tasks.push(function(callback) {
+            revision.save(function(error, revision) {
+                if (error) {
+                    console.error(error);
+                }
 
-        /*
-        x = {
-            minor: 'minor' in rev ? rev.minor : false,
-            revid: 'revid' in rev ? rev.revid : null,
-            parentid: 'parentid' in rev ? rev.parentid : null,
-            size: 'size' in rev ? rev.size : null,
-            suppressed: 'suppressed' in rev,
-            title: article.title,
-            timestamp: rev.timestamp,
-        };
-
-        if ('userhidden' in rev) {
-            revision.anon = null;
-            revision.user = null;
-            revision.userhidden = true;
-        } else if ('anon' in rev) {
-            revision.anon = true;
-            revision.user = rev.user;
-            revision.userhidden = false;
-        } else {
-            revision.anon = false;
-            revision.user = rev.user;
-            revision.userhidden = false;
-        }
-
-        if ('commenthidden' in rev) {
-            revision.commenthidden = true;
-            revision.parsedcomment = null;
-        } else {
-            revision.commenthidden = false;
-            revision.parsedcomment = (
-                'parsedcomment' in rev ? rev.parsedcomment : null);
-        }
-
-        if ('sha1hidden' in rev) {
-            revision.sha1 = null;
-            revision.sha1hidden = true;
-        } else {
-            revision.sha1 = rev.sha1;
-            revision.sha1hidden = false;
-        }
-        */
+                callback(null, revision);
+            });
+        });
     });
+
+    async.parallel(tasks, function(error, results) {
+        console.log("Processed " + article.title + ": inserted " + results.length + " documents.");
+        callback(error, results.length);
+    });
+    // */
 
     /*
     var wikiEndpoint = "https://en.wikipedia.org/w/api.php";
@@ -204,23 +248,19 @@ function processArticleFile(filename, RevisionCls) {
         console.log(revisions);
     });
     // */
-
-    console.log("Processed: " + article.title);
 }
 
 var dataDir = './data'
 var files = fs.readdirSync(dataDir)
-files = files.slice(0, 20);
+var databaseAddress = 'mongodb://localhost/wikipedia_6'
 
-// sync
-
-mongoose.connect('mongodb://localhost/wikipedia_2')
+mongoose.connect(databaseAddress)
 
 // Create a connection to localhost on database called temporary
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', function() {
-    console.log("We are now connected to the database...");
+    winston.info("Connected to %s", databaseAddress);
 
     // Create a schema in mongoDB, and compile it into a model
     var revisionSchema = mongoose.Schema({
@@ -242,12 +282,17 @@ db.once('open', function() {
 
     var Revision = mongoose.model('Revision', revisionSchema);
 
-    // Go through all the files and load each and everyone of them into the
-    // database, and show that we are awesome...
 
+    winston.info("Processing historical archive.");
+
+    // Processing in series otherwise to many asynchronous callbacks are waiting
+    // to run, causing an OOM error.
+    var tasks = [];
     files.forEach(function(filename) {
-        processArticleFile(filename, Revision);
+        tasks.push(function(callback) {
+            processArticleFile(filename, Revision, callback);
+       });
     });
-
+    async.series(tasks);
 });
 
