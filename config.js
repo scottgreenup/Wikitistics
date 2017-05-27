48691d74b8362ea6e7e59e02ac1f76b261a6e750
scottgreenup <scott.j.greenup@gmail.com>
2017-05-28 01:07:59 +1000

    Fills the database with all the latest revisions.

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    database: {
+        hostname: 'mongodb://localhost/wikipedia_8'
+    },
+    dataDir: './data',
+    port: 8000
+}
--- main.js
+++ main.js
@@ -28,6 +28,8 @@ const winston = require('winston')
 
 mongoose.Promise = global.Promise;
 
+const config = require('./config');
+
 var RevisionModel = require('./app/models/revision').model;
 var app = express()
 
@@ -42,13 +44,9 @@ app.get('/', function(req, res) {
 
 })
 
-const PORT = 8000;
-
-app.listen(PORT, function() {
-    console.log('Running on :%d', PORT);
+app.listen(config.port, function() {
+    console.log('Running on :%d', config.port);
 });
-
-var dataDir = './data'
 
 //------------------------------------------------------------------------------
 
@@ -133,135 +131,261 @@ function latestTimestamp(revisions) {
     return {latest: latest, revid: revid}
 }
 
-function processArticleFile(data, title, RevisionModel, callback) {
+function removeFileType(fileName) {
+    return fileName.substring(0, fileName.lastIndexOf('.'));
+}
 
-    // Find all revisions in the database for this article, then see if our
-    // historical data has any elements missing from the database. If it does,
-    // add it.
-    var addHistoricalData = function(callback) {
-        RevisionModel
-            .find({title: title})
-            .sort({"revid": 1})
-            .exec(function(err, revisions) {
-                var i = 0;
-                var tasks = [];
+// Adds the historical data stored in fileName to the database. Validates that
+// a revision doesn't already exist in the database first. This avoids
+// duplicates at all costs; this is a performance hit.
+function addHistoricalData(title, jsonData, callback) {
+    RevisionModel.find(
+        {title: title}
+    ).sort(
+        {'revid': 1}
+    ).exec(function(error, revisions) {
+        var i = 0;
+        var tasks = [];
 
-                data.forEach(function(r) {
-                    while (i < revisions.length && r.revid > revisions[i].revid) {
-                        i++;
+        // Create tasks for adding missing historical data to the database.
+        jsonData.forEach(function(r) {
+
+            // Move our cursor in the revision dataset forward
+            while (i < revisions.length && r.revid > revisions[i].revid) {
+                i++;
+            }
+
+            // Is r already in the database?
+            if (i < revisions.length && r.revid == revisions[i].revid) {
+                return;
+            }
+
+            // Create the task to add it, so we can run them together.
+            tasks.push(function(callback) {
+                var revision = new RevisionModel(r);
+                revision.save(function(error, revision) {
+                    if (error) {
+                        console.error(error);
                     }
 
-                    if (i < revisions.length) {
-                        if (r.revid == revisions[i].revid) {
-                            return;
-                        }
-                    }
-
-                    // add r to the database
-                    tasks.push(function(callback) {
-                        revision = new RevisionModel(r);
-                        revision.save(function(error, revision) {
-                            if (error) {
-                                console.error(error);
-                            }
-
-                            callback(null, revision);
-                        });
-                    });
-                });
-
-                // Run every task in parallel, when they are all finished, execute
-                // the callback for processArticleFile
-                async.parallel(tasks, function(error, results) {
-                    winston.info(
-                        "Processed '%s': inserted %d; found %d existing.",
-                        article.title, results.length, revisions.length);
-                    callback(error, results.length);
+                    callback(null, revision);
                 });
             });
-    }
+        });
 
+        // Run all the tasks and get the list of newly added revisions back
+        async.parallel(tasks, function(error, results) {
+            winston.info(
+                "Processed '%s': inserted %d; found %d existing.",
+                title, results.length, revisions.length);
+            callback(error, results);
+        });
+    });
+}
+
+function filterHistoricalData(title, jsonData, callback) {
     RevisionModel
-        .find({title: title, revid: data[data.length - 1].revid})
+        .find({title: title, revid: jsonData[jsonData.length - 1].revid})
         .exec(function(err, revisions) {
             if (revisions.length == 1) {
                 winston.info("Processed '%s': up to date", title);
                 callback(null, null);
             } else {
-                callback(null, addHistoricalData);
+                callback(null, title);
             }
         });
+}
 
-    /*
-    var wikiEndpoint = "https://en.wikipedia.org/w/api.php";
+function importHistoricalData() {
+    winston.info("Processing historical archive.");
+
+    fileData = new Map();
+
+    fs.readdirSync(config.dataDir).forEach(function(fileName) {
+        var rawData = fs.readFileSync(config.dataDir + '/' + fileName, 'utf8');
+        var jsonData = JSON.parse(rawData);
+        jsonData.sort(function(a, b) {
+            return a.revid - b.revid;
+        });
+        var title = removeFileType(fileName);
+        fileData.set(title, jsonData);
+    });
+
+    var tasks = [];
+
+    fileData.forEach(function(value, key) {
+        tasks.push(function(callback) {
+            filterHistoricalData(key, value, callback);
+        });
+    });
+
+    async.parallel(tasks, function(error, titles) {
+        titles = titles.filter(function(e) { return e !== null; });
+        tasks = [];
+        titles.forEach(function(title) {
+            tasks.push(function(callback) {
+                addHistoricalData(title, fileData.get(title), callback);
+            });
+        });
+        async.series(tasks, function(error, results) {
+            winston.info("Finished importing historical data.");
+        });
+    });
+}
+
+
+function getLatestRevisionInDatabase(title) {
+    return new Promise((resolve, reject) => {
+        RevisionModel
+            .find({title:title})
+            .sort({revid: -1})
+            .limit(1)
+            .exec(function(err, revisions) {
+                if (err) {
+                    reject(err);
+                } else if (revisions.length == 0) {
+                    resolve({title: title, timestamp: 0});
+                } else {
+                    winston.info("%s: %s", title, revisions[0].timestamp);
+                    resolve({
+                        revision: revisions[0],
+                        title: title,
+                        timestamp: revisions[0].timestamp
+                    });
+                }
+            });
+    });
+}
+
+function createQuery(title, timestamp) {
+    const wikiEndpoint = "https://en.wikipedia.org/w/api.php";
     var parameters = [
         'action=query',
         'format=json',
         'prop=revisions',
-        'titles=' + urlencode(article.name),
+        'titles=' + urlencode(title),
         'rvprop=sha1|parsedcomment|size|timestamp|userid|user|ids',
-        'rvlimit=3'
+        'rvlimit=max',
+        'rvstart=' + timestamp,
+        'rvdir=newer'
     ];
-    var url = wikiEndpoint + "?" + parameters.join("&");
-    var options = {
-        'url': url,
+    return {
+        'url': wikiEndpoint + "?" + parameters.join("&"),
         'Accept': 'application/json',
         'Accept-Charset': 'utf-8'
     }
-
-    request(options, function(err, res, data) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-
-        if (res.statusCode !== 200) {
-            console.error("Error code was " + res.statusCode);
-            console.log(data);
-            return;
-        }
-
-        json = JSON.parse(data)
-        pages = json.query.pages
-        revisions = pages[Object.keys(pages)[0]].revisions
-        console.log("There are " + revisions.length + " revisions.");
-        console.log(revisions);
-    });
-    // */
 }
 
-var databaseAddress = 'mongodb://localhost/wikipedia_7'
-mongoose.connect(databaseAddress)
 
-function importHistoricalData(RevisionModel) {
-    winston.info("Processing historical archive.");
+function getLatestRevisionsOnWikipedia(data) {
+    return new Promise((resolve, reject) => {
 
-    var files = fs.readdirSync(dataDir)
-    var tasks = [];
+        var currTimestamp = data.timestamp;
+        var allRevisions = [];
 
-    files.forEach(function(filename) {
-        tasks.push(function(callback) {
-            var rawData = fs.readFileSync(dataDir + '/' + filename, 'utf8');
-            var jsonData = JSON.parse(rawData).sort(function(a, b) {
-                return a.revid - b.revid;
+        function requestSeries(err, res, body) {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            if (res.statusCode !== 200) {
+                reject(new Error("Status code was " + res.statusCode));
+                return;
+            }
+
+            json = JSON.parse(body);
+            pages = json.query.pages;
+            revisions = pages[Object.keys(pages)[0]].revisions;
+            allRevisions = allRevisions.concat(
+                revisions.slice(1, revisions.length));
+
+            if (revisions.length < 500) {
+                if (allRevisions.length > 0) {
+                    winston.info('%s: %s - %s: There are %d revisions.',
+                        data.title,
+                        allRevisions[0].timestamp,
+                        allRevisions[allRevisions.length - 1].timestamp,
+                        allRevisions.length);
+                }
+                data.revisions = allRevisions;
+                resolve(data);
+                return;
+            }
+
+            options = createQuery(
+                data.title,
+                revisions[revisions.length - 1].timestamp);
+            request(options, requestSeries);
+        }
+
+        var options = createQuery(data.title, currTimestamp);
+        request(options, requestSeries);
+    });
+}
+
+function insertRevisionsIntoDatabase(data) {
+    return new Promise((resolve, reject) => {
+
+        if (data.revisions.length == 0) {
+            resolve(null);
+            return;
+        }
+
+        var tasks = [];
+
+        data.revisions.forEach(function(revision) {
+            tasks.push(function(callback) {
+                revision.title = data.title;
+                (new RevisionModel(revision)).save(function(error, r) {
+                    if (error) {
+                        winston.error(error);
+                        callback(null, null);
+                    } else {
+                        callback(null, revision);
+                    }
+                });
             });
-            var title = filename.substring(0, filename.lastIndexOf('.'));
-            processArticleFile(jsonData, title, RevisionModel, callback);
-       });
-    });
+        });
 
-    async.parallel(tasks, function(error, insertTasks) {
-        insertTasks = insertTasks.filter(function(e) { return e != null });
-        async.series(insertTasks);
+        async.parallel(tasks, function(error, results) {
+            results = results.filter(function(e) { return e !== null; });
+            winston.info("%s: inserted %d new revisions.",
+                data.title, results.length);
+            resolve(results);
+        });
     });
 }
 
-// Create a connection to localhost on database called temporary
+
+function getLatestRevisions(title) {
+    return getLatestRevisionInDatabase(title)
+        .then(getLatestRevisionsOnWikipedia)
+        .then(insertRevisionsIntoDatabase);
+}
+
+mongoose.connect(config.database.hostname)
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
 db.once('open', function() {
-    winston.info("Connected to %s", databaseAddress);
+    winston.info("Connected to %s", config.database.hostname);
 
-    importHistoricalData(RevisionModel);
+    //importHistoricalData();
+
+            var tasks = [];
+
+    RevisionModel.find({}).distinct('title', function(error, titles) {
+
+        function perform(index) {
+            if (index < titles.length) {
+                getLatestRevisions(titles[index]).then(function(results) {
+                    perform(index + 1);
+                });
+            }
+        }
+
+        perform(0);
+    });
+
 });
 
