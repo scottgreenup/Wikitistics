0a61bc6e3b04780e85c63e29a5ca68c47c86da01
scottgreenup <scott.j.greenup@gmail.com>
2017-05-28 19:17:05 +1000

    /individual/revisionCount complete

--- app/controllers/individual.js
+++ app/controllers/individual.js
@@ -1,5 +1,11 @@
 
 const express = require('express');
+const fs = require('fs');
+const mongoose = require('mongoose');
+const util = require('util');
+const winston = require('winston');
+
+var RevisionModel = require('../models/revision').model;
 
 module.exports.showPage = function(req, res) {
     content = {
@@ -8,3 +14,70 @@ module.exports.showPage = function(req, res) {
 
     res.render('individual', content);
 }
+
+module.exports.articleList = function(req, res) {
+
+    RevisionModel.aggregate([
+        {
+
+            $group: {
+                _id: '$title'
+            }
+        }
+    ], function(err, docs) {
+        var articleList = [];
+        docs.forEach(function(doc) {
+            articleList.push(doc._id);
+        });
+        res.send(articleList);
+    });
+
+}
+
+module.exports.revisionCount = function(req, res) {
+    if (!req.query.article) {
+        res.send("failure");
+        return;
+    }
+
+    var articleTitle = req.query.article;
+
+    RevisionModel
+        .find({title: articleTitle})
+        .count(function(err, count) {
+            if (err) {
+                res.send("0");
+            } else {
+                res.send("" + count);
+            }
+        });
+}
+
+module.exports.topFiveUsers = function(req, res) {
+    if (!req.query.article) {
+        res.send("failure");
+        return;
+    }
+}
+
+module.exports.byYearByUser = function(req, res) {
+    if (!req.query.article) {
+        res.send("failure");
+        return;
+    }
+}
+
+module.exports.byUser = function(req, res) {
+    if (!req.query.article) {
+        res.send("failure");
+        return;
+    }
+}
+
+module.exports.byYearByTopFive = function(req, res) {
+    if (!req.query.article) {
+        res.send("failure");
+        return;
+    }
+}
+
--- app/routes/individual.js
+++ app/routes/individual.js
@@ -5,5 +5,7 @@ const controller = require('../controllers/individual')
 const router = express.Router();
 
 router.get('/', controller.showPage);
+router.get('/articleList', controller.articleList);
+router.get('/revisionCount', controller.revisionCount);
 
 module.exports = router;
